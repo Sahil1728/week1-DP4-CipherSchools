c01defe8391ac441ee4c2a20d87df06bb3ad15b5
Sahil1728 <77491529+Sahil1728@users.noreply.github.com>
2023-01-25 18:37:30 +0000

    Wrote mergeArrays function & printed sorted array

--- 1_merge sorted arrayq1.cpp
+++ 1_merge sorted arrayq1.cpp
@@ -3,6 +3,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// merging the two arrays using merge function
+vector<int> mergeArrays(vector<int> &a, int m, vector<int> &b, int n){
+    // Size of a, b and merged array
+    vector<int> nArr(m+n, 0);
+    int i = m-1;
+    int j = n-1;
+    int k = m+n-1;
+    // while both iterators are >=0 i.e neither of them is fully traversed
+    while(i>=0 && j>=0){
+        // if a[i]>b[j] => a[k]=a[i]
+        if(a[i]>b[j]){
+            nArr[k]=a[i];
+            i--;
+        }
+        else{
+            nArr[k]=b[j];
+            j--;
+        }
+        k--;
+    }
+    // if the second array isn't fully traversed
+    while(j>=0){
+        nArr[k]=b[j];
+        j--;
+        k--;
+    }
+    // if the second array isn't fully traversed
+    while(i>=0){
+        nArr[k]=a[i];
+        i--;
+        k--;
+    }
+    return nArr;
+}
+
+
+
 int main(){
     // m->len of array 1
     // n->len of array 2
@@ -17,9 +54,16 @@ int main(){
         a.push_back(x);
     }
     cout<<endl<<"Enter the second sorted array ";
-    for(int i=0;i<m;i++){
+    for(int i=0;i<n;i++){
         int x;cin>>x;
-        a.push_back(x);
+        b.push_back(x);
+    }
+    // calling the mergeArrays function and passing both the arrays to create new sorted array
+    vector<int> newArr = mergeArrays(a, m, b, n);
+    
+    // printing the elements of new array to check whether the code worked or not
+    for(auto x:newArr){
+        cout<<x<<" ";
     }
 
     return 0;
