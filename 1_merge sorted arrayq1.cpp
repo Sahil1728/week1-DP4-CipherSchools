259d53023cdef6834ed3874f1185e2ce9a1ea0f7
Sahil1728 <77491529+Sahil1728@users.noreply.github.com>
2023-01-25 18:13:54 +0000

    Taking both arrays as input in form of vectors

--- 1_merge sorted arrayq1.cpp
+++ 1_merge sorted arrayq1.cpp
@@ -4,6 +4,23 @@
 using namespace std;
 
 int main(){
+    // m->len of array 1
+    // n->len of array 2
+    int m,n;
+    cout<<"Enter the length of the first and the second array respectively ";
+    cin>>m>>n;
+    vector<int>a;
+    vector<int>b;
+    cout<<endl<<"Enter the first sorted array ";
+    for(int i=0;i<m;i++){
+        int x;cin>>x;
+        a.push_back(x);
+    }
+    cout<<endl<<"Enter the second sorted array ";
+    for(int i=0;i<m;i++){
+        int x;cin>>x;
+        a.push_back(x);
+    }
 
     return 0;
 }
