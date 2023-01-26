2a022f415971d744022c21eb1d39a657cd0982c2
Sahil1728 <77491529+Sahil1728@users.noreply.github.com>
2023-01-26 12:54:20 +0000

    Added code of main() function containing the array

--- 2_mergeSortedArray2.cpp
+++ 2_mergeSortedArray2.cpp
@@ -2,3 +2,12 @@
 // Given a sorted array return the indexes of first and last occurence of given value
 
 // Approach-> We will be using binary search for finding the indexes and Time Complexity for the same will be O(logN)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // given the sorted array
+    vector<int> sortedArr{1, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 6, 6, 7, 7, 8, 8, 8, 8, 9, 9};
+    return 0;
+}
