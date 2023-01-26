c292175b68df11ac738d8e37088f4027a7959b24
Sahil1728 <77491529+Sahil1728@users.noreply.github.com>
2023-01-26 13:42:05 +0000

    Taking matrix as input and printing it

--- 3_matrixRotation.cpp
+++ 3_matrixRotation.cpp
@@ -1,4 +1,38 @@
 // Leetcode link of problem
 // https://leetcode.com/problems/rotate-image/
 
-// APPROACH-> take transpose of the matrix and the rotate the columns n/2 times 
+// APPROACH-> take transpose of the matrix and the rotate the columns n/2 times
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // Since we have to rotate a n*n matrix we will take n as input
+    cout<<"Enter the size of the square matrix ";
+    int n;cin>>n;
+    // the matrix
+    vector<vector<int>> matrix;
+    // taking the matrix as input
+    for(int i=0;i<n;i++){
+        cout<<"Enter row "<<i+1<<endl;
+        vector<int> row;
+        for(int j=0;j<n;j++){
+            cout<<"Enter element "<<j+1<<": ";
+            int x;cin>>x;
+            row.push_back(x);
+        }
+        matrix.push_back(row);
+        cout<<endl;
+    }
+    // printing the matrix
+    for(auto x:matrix){
+        for(auto y:x){
+            cout<<y<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
+
+}
