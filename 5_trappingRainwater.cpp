a12cac57db6e3078e7f1b9ef665a656e7ee88013
Sahil1728 <77491529+Sahil1728@users.noreply.github.com>
2023-01-27 11:55:59 +0000

    Code for taking heights of blocks as input

--- 5_trappingRainwater.cpp
+++ 5_trappingRainwater.cpp
@@ -1,2 +1,20 @@
 // Leetcode link of problem
 // https://leetcode.com/problems/trapping-rain-water/
+
+// We have to return trh number of units of water which will get stored between the blocks
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+    cout<<"Enter the total number of blocks: ";
+    int n;cin>>n;
+    cout<<endl<<"Enter the heights of blocks [0,infinty): ";
+    vector<int> heights;
+    for(int i=0;i<n;i++){
+        int x;cin>>x;
+        heights.push_back(x);
+    }
+}
