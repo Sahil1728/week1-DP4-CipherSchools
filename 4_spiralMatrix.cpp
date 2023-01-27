f256da930498a5ecae8e912f3269d942118eb7cb
Sahil1728 <77491529+Sahil1728@users.noreply.github.com>
2023-01-27 07:09:33 +0000

    Code for inpit and printing matrix

--- 4_spiralMatrix.cpp
+++ 4_spiralMatrix.cpp
@@ -5,3 +5,32 @@
 // create variable sRow, sCol, eRow, eCol variables and iterate, update them accordingly to get the desired result
 
 
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    cout<<"Enter size of n*n matrix: ";
+    int n;cin>>n;
+    vector<vector<int>> matrix;
+    for(int i=0;i<n;i++){
+        cout<<"Enter row "<<i+1<<endl;
+        vector<int> row;
+        for(int j=0;j<n;j++){
+            cout<<"Enter element "<<j+1<<": ";
+            int x;cin>>x;
+            row.push_back(x);
+        }
+        matrix.push_back(row);
+        cout<<endl;
+    }
+    // printing the matrix
+    cout<<"Original matrix:"<<endl;
+    for(auto x:matrix){
+        for(auto y:x){
+            cout<<y<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
