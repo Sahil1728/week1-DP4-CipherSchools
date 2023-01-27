12fb60b4f0148d494949fd44669ee493a29d270b
Sahil1728 <77491529+Sahil1728@users.noreply.github.com>
2023-01-27 07:06:12 +0000

    Added approach

--- 4_spiralMatrix.cpp
+++ 4_spiralMatrix.cpp
@@ -1,2 +1,7 @@
 // Leetcode link of problem
 // https://leetcode.com/problems/spiral-matrix/
+
+// Approach
+// create variable sRow, sCol, eRow, eCol variables and iterate, update them accordingly to get the desired result
+
+
